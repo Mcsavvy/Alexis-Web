06b8bc90e62027933ff69896bd3605b0e0a121cd
Isaac Olumide Ogunfolaju <zicogunfolaju@gmail.com>
2024-03-29 18:04:16 +0000

    removed auth routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import PrivacyPolicy from './components/PrivacyPolicy';
-import { useEffect } from "react";
-import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
-
-
-function Login() {
-  const {login} = useKindeAuth();
-
-  useEffect(() => {
-    login();
-  }, [login]);
-
-  return <div>Login</div>;
-}
-
-function Register() {
-  const {register} = useKindeAuth();
-
-  useEffect(() => {
-    register();
-  }, [register]);
-  return <div>Register</div>;
-}
-
-function Logout() {
-  const {logout} = useKindeAuth();
-
-  useEffect(() => {
-    logout();
-  }, [logout]);
-
-  return <div>Logout</div>;
-}
-
 
 const router = createBrowserRouter([
   { path: "/", element: <LandingPage/>, index: true },
   { path: "/privacy-policy", element: <PrivacyPolicy />},
-  { path: "/login", element: <Login /> },
-  { path: "/register", element: <Register /> },
-  { path: "/logout", element: <Logout /> },
 ])
 
 
